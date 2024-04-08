a0071d93be6382786ca59ef5d91c3a1f00213335
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2024-04-08 11:46:12 +0530

    fix a issue in 7-rotate-arrays-by-k file

--- 2-Arrays/7-rotate-arrays-by-k.js
+++ 2-Arrays/7-rotate-arrays-by-k.js
@@ -30,7 +30,7 @@ console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3));
 function rotateArrayOptimised(nums, k) {
   let size = nums.length;
 
-  if (size > k) {
+  if (k > size) {
     k = k % size;
   }
   //[1, 2, 3, 4, 5, 6, 7] => [7,6,5,4,3,2,1] => [5,6,7,4,3,2,1] => [5,6,7,1,2,3,4]
