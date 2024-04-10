7aff00a899a5410b7ee2f424920cc0a93010cf3c
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2024-04-10 13:31:43 +0530

    Recursion : fixed a issue in Range of numbers question

--- 4-Recursion/3-Range-of-numbers.js
+++ 4-Recursion/3-Range-of-numbers.js
@@ -11,11 +11,11 @@ function rangeOfNumbers(startNum, endNum) {
   }
 }
 
-// rangeOfNumbers(1,5) => [1,2,3,4,5]
-// rangeOfNumbers(1,4) => [1,2,3,4]
-// rangeOfNumbers(1,3) => [1,2,3]
-// rangeOfNumbers(1,2) => [1,2]
-// rangeOfNumbers(1,1) => [1]
-// rangeOfNumbers(1,0) => []
+// rangeOfNumbers(0,5) => [1,2,3,4,5]
+// rangeOfNumbers(0,4) => [1,2,3,4]
+// rangeOfNumbers(0,3) => [1,2,3]
+// rangeOfNumbers(0,2) => [1,2]
+// rangeOfNumbers(0,1) => [1]
+// rangeOfNumbers(0,0) => []
 
 console.log(rangeOfNumbers(0, 5));
