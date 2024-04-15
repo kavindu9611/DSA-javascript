1bcda4f910d003112344500e6453128fec7896d8
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2024-04-15 23:39:04 +0530

    Recursion Questions: Palindrome

---
+++ 4-Recursion/4-Palindrome.js
@@ -0,0 +1,18 @@
+//Ques 1 - Palindrome Number
+//An integer is a palindrome when it reads the same forward and backward
+
+//Input: x = 121 ---------->>>> Output:true
+//Input: x = 10 ----------->>>> Output:false
+
+const isPalindrome = function (x) {
+  return x < 0 ? false : x === +x.toString().split("").reverse().join("");
+};
+
+const res = isPalindrome(-121);
+console.log(res);
+
+//121 => "121" => ["1","2","1"] =>  ["1","2","1"] => "121"
+
+//x ------->>> convert string into number if string has number
+//console.log(+"b")----------> Output: NaN
+//console.log(+"11")----------> Output:11
